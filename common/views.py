108e0a2850f2b7f52ecedc8c2b55b1761b7ee9b2
adithya010103 <adithyaev@interns.cybersquare.org>
2023-12-21 13:26:22 +0530

    listed tasks and implemented edit task

--- common/views.py
+++ common/views.py
@@ -5,22 +5,30 @@ from common.models import User
 # Create your views here.
 def register(request):
     msg = ''
+    
+    # check if the form is submitted
     if request.method == 'POST':
+        # retrieve data sent from the form and store in a variable
         user_name = request.POST['name']
         user_email = request.POST['email']
         user_phone = request.POST['contact']
         user_password = request.POST['password']
         
+        
+        # check if email already exists
         if User.objects.filter(email=user_email).exists():
             msg = 'email already exists'
             return render(request,'common/register.html', {"status" : msg })
         
+        # crate a new user(an instance)
         user = User(
             name = user_name,
             email = user_email,
             mobile = user_phone,
             password = user_password 
         )
+        
+        # insert into table(in to database)
         user.save()
         msg = 'user registered successfully'
     return render(request,'common/register.html', {"status" : msg })
